b7ebaecfe761a4a3bb996835b133630469d85573
Daniela Nogueira Rampim <daninogueira.dev@gmail.com>
2022-07-18 16:05:33 -0300

    update

---
+++ Java/aulaRetornos/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Quadrilátero
+        System.out.println("Exercício quadrilátero");
+
+        System.out.println("Àrea do quadrado: " + Quadrilatero.area(3));
+        System.out.println("Área do retangulo: " + Quadrilatero.area(5d, 10d));
+        System.out.println("Área do trapézio: " + Quadrilatero.area(7, 8, 9));
+        System.out.println("Área do losangulo: " + Quadrilatero.area(5f, 5f));
+    }
+
+}
