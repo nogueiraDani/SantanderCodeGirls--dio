5eecdf7b74c08cdb01f25b549e6a6b42e80e4324
Daniela Nogueira Rampim <daninogueira.dev@gmail.com>
2022-07-21 09:48:42 -0300

    update

---
+++ Java/desafiosDeCodigo/Intervalo/src/Main.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Main {
+
+    /*
+
+    Desafio
+    Faça um programa que leia um valor qualquer e apresente uma mensagem dizendo em qual dos seguintes intervalos
+    ([0,25], (25,50], (50,75], (75,100]) este valor se encontra. Caso o valor não esteja em nenhum destes intervalos,
+    deverá ser impressa a mensagem: “Fora de intervalo”.
+
+    O símbolo ( representa "maior que". Por exemplo:
+    [0,25]  indica valores entre 0 e 25.0000, inclusive eles.
+    (25,50] indica valores maiores que 25 Ex: 25.00001 até o valor 50.0000000
+
+    Entrada
+    O arquivo de entrada contém um número com ponto flutuante qualquer.
+
+    Saída
+    A saída deve ser uma mensagem conforme exemplo abaixo.
+
+     */
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite um valor a conferir:");
+        String input = scanner.next();
+        double value = Double.parseDouble(input.replace(",","."));
+
+        if (value >= 0 && value <= 25) {
+            System.out.println("Intervalo [0,25]");
+        } else if (value > 25 && value <= 50) {
+            System.out.println("Intervalo (25,50]");
+        } else if (value > 50 && value <= 75) {
+            System.out.println("Intervalo (50,75]");
+        } else if (value > 75 && value <= 100) {
+            System.out.println("Intervalo (75,100]");
+        } else {
+            System.out.println("Fora de intervalo");
+        }
+    }
+}
+
+
